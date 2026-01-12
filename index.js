da887253a05c08806f7f4540736b85376dd6d498
Rafael Andriotti <rafael.rebelo47@gmail.com>
2026-01-12 16:33:18 -0300

    Adicionando funcionalidades

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Importando os modulos 
+<<<<<<< HEAD
 const validarSenhaEEmail = require('./src/validarSenhaEEmail')
 const criarNumerosAleatorios = require('./src/criarNumerosAleatorios')
+=======
+import { validaEmail, validaSenha } from './src/validarSenhaEEmail'
+>>>>>>> cf3c3d7 (adicionando funcoes em arrays e tratando erros)
 const trocarFormatoStrings = require('./src/trocarFormatoString')
 
 module.exports = { validarSenhaEEmail, criarNumerosAleatorios, trocarFormatoStrings }
